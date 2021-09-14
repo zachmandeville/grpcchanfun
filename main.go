25394f28aed46f3a95241b07e19e369a3ba92df6
workzach mandeville <workzach@Hippies-MacBook-Air.local>
2021-09-14 14:07:12 +1200

    simplify and improve our loops

--- main.go
+++ main.go
@@ -4,9 +4,9 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"math/rand"
 	"io"
 	"log"
+	"math/rand"
 	"sync"
 	"time"
 
@@ -18,24 +18,27 @@ var (
 	serverAddr = flag.String("server_addr", "localhost:10000", "The server address")
 )
 
-func runSquares (client mathspb.MathsClient, nums chan int32, ch chan string, done chan bool) {
+func runSquares(client mathspb.MathsClient, nums chan int32, ch chan string) {
 	// run a bidirectional stream that will sends the server response to the ch channel.
 	// the sending stream will only close when it receive no more messages across the numschannel
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
+
 	stream, err := client.Squares(ctx)
 	if err != nil {
 		log.Fatalf("%v.Squares(_) = _, %v", client, err)
 	}
+
+	// receiving stream for our gRPC server
+	// will loop until the sending channel is closed.
 	var wg sync.WaitGroup
 	go func() {
 		for {
-		wg.Add(1)
+			wg.Add(1)
 			in, err := stream.Recv()
 			if err == io.EOF {
-				close(ch)
 				log.Println("no more numbers from server")
-				done <- true
+				close(ch)
 				return
 			}
 			if err != nil {
@@ -47,8 +50,8 @@ func runSquares (client mathspb.MathsClient, nums chan int32, ch chan string, do
 		}
 	}()
 
+	// Sending stream.
 	// this for loop will run until the num channel closes.
-	// this function doesn't control whether the num channel closes,
 	// that is handled outside this function.
 	for num := range nums {
 		log.Printf("received %v from channel\n", num)
@@ -61,10 +64,10 @@ func runSquares (client mathspb.MathsClient, nums chan int32, ch chan string, do
 	}
 	log.Println("number loop ended")
 	stream.CloseSend()
-	wg.Wait() // keep the for loops running until we are fully done sending all our numbers to the server.
+	wg.Wait() // keep the for loops running until we've received all squares from our server.
 }
 
-func main () {
+func main() {
 	conn, err := grpc.Dial(*serverAddr, grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("unable to connect to server: %v", err)
@@ -72,19 +75,18 @@ func main () {
 	defer conn.Close()
 
 	client := mathspb.NewMathsClient(conn)
+
 	ch := make(chan string, 2)
-	done := make(chan bool, 2)
 	nums := make(chan int32, 2)
+	go runSquares(client, nums, ch) // this is how we interact with the external server, like a function that interacts with an xDS server
 
-	go runSquares(client, nums, ch, done) // this is how we interact with the external server, like a function that intereacts with an xDS server
-
-	go func () {
+	go func() {
 		// here we have a concurrent routine that is random, and so we can't anticipate when the number channel will close.
 		// we want to make sure we have the program setup well enough that we never try to send across a closed channel
-		// and we get all our responses...even if we odn't know how many these would be.
+		// and we get all our responses...even if we don't know how many these would be.
 		for {
-		  rand.Seed(time.Now().UnixNano())
-		  n := rand.Intn(12 - 1) + 1
+			rand.Seed(time.Now().UnixNano())
+			n := rand.Intn(12-1) + 1
 			if n == 7 {
 				log.Println("sending lucky number 7 across channel")
 				nums <- int32(n)
@@ -92,23 +94,14 @@ func main () {
 				return
 
 			} else {
-				log.Printf("sending %v across channel\n",n)
+				log.Printf("sending %v across channel\n", n)
 				nums <- int32(n)
 			}
 		}
 	}()
 
-	// keep the for loop running until we get a done signal from our runSquares program.  Without this, the program exits too quickly.
-	// the messages received will be pulled from our server.
-	for {
-		select {
-		case msg := <- ch:
-			if len(msg) > 0 {
-				fmt.Println(msg)
-			}
-		case <-done:
-			fmt.Println("All done!!")
-			return
-		}
+	// keep the for loop going until the ch channel is closed,
+	for msg := range ch {
+		fmt.Println(msg)
 	}
 }
